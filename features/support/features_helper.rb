81a13deb5693f42e142792e9cb245e71cd8a4404
Daniel <robotright@gmail.com>
2012-09-05 06:08:01 +0800

    comment code

--- features/support/features_helper.rb
+++ features/support/features_helper.rb
@@ -1,5 +1,7 @@
 ARUBA_DIR = 'tmp/aruba'
 
+# helper method for performing file operations in steps
+# aruba defaults to using 'tmp/aruba'
 def in_aruba_dir(file_dir)
   dir = File.dirname(file_dir)
   working_dir = File.join(ARUBA_DIR, dir)
