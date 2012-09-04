47e8a6fcbf69af1af8f715f3fc18b9d45543246b
Daniel <robotright@gmail.com>
2012-09-05 06:16:06 +0800

    remove LegalWriter and move code into SGETagger

--- lib/sge_tagger.rb
+++ lib/sge_tagger.rb
@@ -1,12 +1,27 @@
 # encoding: utf-8
 require 'methadone'
 require "sge_tagger/version"
-require "sge_tagger/legal_writer"
-
 
 module SGETagger
   LEGAL_INFO = <<-EOS
 Confidential information of Sleepy Giant Entertainment, Inc.
 © Sleepy Giant Entertainment, Inc.
   EOS
+
+  def self.tag_legal(rfile)
+    og_text = File.read(rfile)
+
+    File.open(rfile, 'w') do |wfile|
+      legal_text = LEGAL_INFO
+
+      case File.extname(rfile)
+      when '.rb'
+        legal_text.insert(0, "=begin\n")
+        legal_text << "=end\n"
+      end
+
+      wfile.puts legal_text
+      wfile.puts og_text
+    end
+  end
 end
