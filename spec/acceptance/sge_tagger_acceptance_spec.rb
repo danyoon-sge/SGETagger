e75aeb23de9fe7b30d574e0028bf04d92bf3bf86
Daniel <robotright@gmail.com>
2012-09-04 18:05:07 +0800

    working version for simplest case

--- spec/acceptance/sge_tagger_acceptance_spec.rb
+++ spec/acceptance/sge_tagger_acceptance_spec.rb
@@ -15,4 +15,36 @@ describe SGETagger do
       code.should_not == 0
     end
   end
+
+  context 'dir /tmp with SGE owned files' do
+    before(:each) do
+      # setup the tmp dir with files
+      FILES = %w[sge.sass sge.less sge.css sge.html sge.html.haml sge.html.erb sge.rb sge.js]
+
+      rm_rf TEMP_DIR
+      mkdir TEMP_DIR
+
+      Dir.chdir(TEMP_DIR) do
+        FILES.each do |fname|
+          sh "echo 'SGE owned file' >> #{fname}"
+        end
+      end
+    end
+
+    after(:each) do
+      rm_rf TEMP_DIR
+    end
+
+    describe "execute 'sge_tagger tmp'" do
+      it 'tags SGE files with legal info' do
+        sh "bundle exec bin/sge_tagger #{TEMP_DIR}"
+        Dir.chdir(TEMP_DIR) do
+          Dir["**/*"].each do |file|
+            text = File.read(file)
+            text.include?('some legal stuff').should be_true
+          end
+        end
+      end
+    end
+  end
 end
