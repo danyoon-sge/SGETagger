33474094f6154fec3314fff2b8951274d360979a
Daniel <robotright@gmail.com>
2012-09-05 07:28:08 +0800

    extract method for tagging text

--- lib/sge_tagger.rb
+++ lib/sge_tagger.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'methadone'
 require "sge_tagger/version"
+require 'pry'
 
 module SGETagger
   LEGAL_INFO = <<-EOS
@@ -8,20 +9,25 @@ Confidential information of Sleepy Giant Entertainment, Inc.
 © Sleepy Giant Entertainment, Inc.
   EOS
 
-  def self.tag_legal(rfile)
+  def self.tag_file(rfile)
     og_text = File.read(rfile)
+    tagged_text = tag_text(og_text, File.extname(rfile))
 
     File.open(rfile, 'w') do |wfile|
-      legal_text = LEGAL_INFO
-
-      case File.extname(rfile)
-      when '.rb'
-        legal_text.insert(0, "=begin\n")
-        legal_text << "=end\n"
-      end
-
-      wfile.puts legal_text
-      wfile.puts og_text
+      wfile.puts tagged_text
     end
+  end
+
+  def self.tag_text(og_text, file_type)
+    tagged_text = LEGAL_INFO
+
+    case file_type
+    when '.rb'
+      tagged_text.insert(0, "=begin\n")
+      tagged_text << "=end\n"
+    end
+
+    tagged_text << og_text
+    tagged_text
   end
 end
