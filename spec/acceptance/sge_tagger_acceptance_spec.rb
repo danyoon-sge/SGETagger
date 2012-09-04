7a46ce3e3c03924ad2f2e6cc4b8077cd78492461
Daniel <robotright@gmail.com>
2012-09-04 17:56:30 +0800

    simpler way to test with dir that does not exist

--- spec/acceptance/sge_tagger_acceptance_spec.rb
+++ spec/acceptance/sge_tagger_acceptance_spec.rb
@@ -9,15 +9,9 @@ include FileUtils
 TEMP_DIR = 'tmp'
 
 describe SGETagger do
-  context 'dir /tmp does not exist' do
-    before(:all) do
-      if Dir.exists?(TEMP_DIR)
-        rm_rf(TEMP_DIR)
-      end
-    end
-
+  context 'passed in dir does not exist' do
     it 'exits with error' do
-      code = sh "bundle exec sge_tagger #{TEMP_DIR}"
+      code = sh "bundle exec sge_tagger non_existing_dir"
       code.should_not == 0
     end
   end
