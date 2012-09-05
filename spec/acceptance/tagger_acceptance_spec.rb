3e49d612e9c9e1c0c52e1216551710f767d39d24
Daniel <robotright@gmail.com>
2012-09-05 15:05:16 +0800

    spec for file within a dir

--- spec/acceptance/tagger_acceptance_spec.rb
+++ spec/acceptance/tagger_acceptance_spec.rb
@@ -5,25 +5,39 @@ include SGETagger
 describe Tagger do
   TEMP_DIR = 'tmp'
 
-  before(:each) do
-    # make a dir containing one file and one dir
-    FileUtils.rm_rf(TEMP_DIR)
-    Dir.mkdir TEMP_DIR
-    Dir.chdir TEMP_DIR do
-      FileUtils.rm_rf("level_one")
-      File.open('sge.txt', 'w+') { |file| file << "sge owned" }
-      Dir.mkdir("level_one")
+  context 'tmp dir containing one file and one dir' do
+    before(:each) do
+      # make a dir containing one file and one dir
+      FileUtils.rm_rf(TEMP_DIR)
+      Dir.mkdir TEMP_DIR
+      Dir.chdir TEMP_DIR do
+        File.open('sge.txt', 'w+') { |file| file << "sge owned" }
+        Dir.mkdir("level_one")
+      end
     end
-  end
 
-  after(:each) do
-    FileUtils.rm_rf(TEMP_DIR)
-  end
+    after(:each) do
+      Dir.chdir '..'
+      FileUtils.rm_rf(TEMP_DIR)
+    end
 
-  describe "#taggable_files" do
-    it 'does not count directories' do
-      tf = Tagger.taggable_files(TEMP_DIR)
-      tf.count.should == 1
+    describe "#taggable_files" do
+      it 'does not count directories' do
+        tf = Tagger.taggable_files(TEMP_DIR)
+        tf.count.should == 1
+      end
+
+      context 'file nested in a dir' do
+        it 'includes files in nested dir' do
+          # add folder one level down
+          file = File.join(TEMP_DIR, "level_one")
+          file = File.join(file, "sge_inside_folder.txt")
+          File.open(file, 'w+') { |f| f << "sge inside folder" }
+
+          tf = Tagger.taggable_files(TEMP_DIR)
+          tf.count.should == 2
+        end
+      end
     end
   end
 
