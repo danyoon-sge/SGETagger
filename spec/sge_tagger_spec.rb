0ee8580ce1170fc27ba4d81b6e7258bf2561b47d
Daniel <robotright@gmail.com>
2012-09-05 11:43:30 +0800

    add happy path specs for text tagging

--- spec/sge_tagger_spec.rb
+++ spec/sge_tagger_spec.rb
@@ -4,6 +4,21 @@ describe SGETagger::Tagger do
   describe "#tag_text(og_text, file_type)" do
     let(:tagger) { SGETagger::Tagger.new }
 
+    context 'untagged file' do
+      it 'tags file' do
+        og_text = "text in file"
+        tagged_text = tagger.tag_text(og_text, '.txt')
+        tagged_text.scan(SGETagger::LEGAL_INFO).count.should == 1
+      end
+
+      it 'properly tags .rb files' do
+        og_text = "text in file"
+        tagged_text = tagger.tag_text(og_text, '.rb')
+        tagged_text.scan(SGETagger::LEGAL_INFO).count.should == 1
+        tagged_text.index("=begin").should == 0
+      end
+    end
+
     context 'already tagged file' do
       it 'only tags once' do
         og_text = SGETagger::LEGAL_INFO
