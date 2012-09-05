adcc584a89ad57827c9426b431f1d5b88bf0603c
Daniel <robotright@gmail.com>
2012-09-05 15:07:58 +0800

    use constant var

--- spec/acceptance/tagger_acceptance_spec.rb
+++ spec/acceptance/tagger_acceptance_spec.rb
@@ -4,6 +4,7 @@ include SGETagger
 
 describe Tagger do
   TEMP_DIR = 'tmp'
+  DIR1 = 'dir1'
 
   context 'tmp dir containing one file and one dir' do
     before(:each) do
@@ -12,7 +13,31 @@ describe Tagger do
       Dir.mkdir TEMP_DIR
       Dir.chdir TEMP_DIR do
         File.open('sge.txt', 'w+') { |file| file << "sge owned" }
-        Dir.mkdir("level_one")
+        Dir.mkdir(DIR1)
+      end
+    end
+
+    after(:each) do
+      Dir.chdir '..'
+      FileUtils.rm_rf(TEMP_DIR)
+    end
+
+    describe "#taggable_files" do
+      it 'does not count directories' do
+        tf = Tagger.taggable_files(TEMP_DIR)
+        tf.count.should == 1
+      end
+
+      context 'file nested in a dir' do
+        it 'includes files in nested dir' do
+          # add folder one level down
+          file = File.join(TEMP_DIR, DIR1)
+          file = File.join(file, "sge_inside_folder.txt")
+          File.open(file, 'w+') { |f| f << "sge inside folder" }
+
+          tf = Tagger.taggable_files(TEMP_DIR)
+          tf.count.should == 2
+        end
       end
     end
 
@@ -38,18 +63,6 @@ describe Tagger do
           tf.count.should == 2
         end
       end
-    end
-  end
-
-  context 'file nested in a dir' do
-    it 'includes files in nested dir' do
-      # add folder one level down
-      file = File.join(TEMP_DIR, "level_one")
-      file = File.join(file, "sge_inside_folder.txt")
-      File.open(file, 'w+') { |f| f << "sge inside folder" }
-
-      tf = Tagger.taggable_files(TEMP_DIR)
-      tf.count.should == 2
     end
   end
 end
