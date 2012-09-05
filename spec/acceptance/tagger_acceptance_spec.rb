156d390436ad600d2dd672489af6e4e2959b0dc3
Daniel <robotright@gmail.com>
2012-09-05 15:34:20 +0800

    messed up some commits while trying to keep them atomic :(

--- spec/acceptance/tagger_acceptance_spec.rb
+++ spec/acceptance/tagger_acceptance_spec.rb
@@ -40,29 +40,5 @@ describe Tagger do
         end
       end
     end
-
-    after(:each) do
-      Dir.chdir '..'
-      FileUtils.rm_rf(TEMP_DIR)
-    end
-
-    describe "#taggable_files" do
-      it 'does not count directories' do
-        tf = Tagger.taggable_files(TEMP_DIR)
-        tf.count.should == 1
-      end
-
-      context 'file nested in a dir' do
-        it 'includes files in nested dir' do
-          # add folder one level down
-          file = File.join(TEMP_DIR, DIR1)
-          file = File.join(file, "sge_inside_folder.txt")
-          File.open(file, 'w+') { |f| f << "sge inside folder" }
-
-          tf = Tagger.taggable_files(TEMP_DIR)
-          tf.count.should == 2
-        end
-      end
-    end
   end
 end
